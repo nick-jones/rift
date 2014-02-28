3092d76219632fbeb190d8f857ff543ad01e6e3c
Nicholas Jones <me@nicholasjon.es>
2014-02-28 11:31:04 +0000

    CLI entry point for basic testing of the library

---
+++ lib/rift/cli.rb
@@ -0,0 +1,21 @@
+require "yaml"
+require "rift/parser"
+
+module Rift
+  class CLI
+    # Entry point for executing CLI commands.
+    def self.run(arguments, parser = Rift::Parser.new)
+      file_path = arguments[0]
+      raise "A file path must be supplied" unless file_path
+
+      exists = File.exist?(file_path)
+      raise "A valid file path must be supplied" unless exists
+
+      file = File.open(file_path, "rb")
+      nodes = parser.parse(file.read)
+
+      YAML::ENGINE.yamler = 'syck'
+      $stdout.puts nodes.to_yaml
+    end
+  end
+end
